7b66af3d89861ced72ce5d5e03a85862bcf0d7de
Syuugo <pub@s1204.me>
2024-04-17 02:26:09 +0900

    Turn off strict mode

--- disabled-remover.user.js
+++ disabled-remover.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Disabled Remover
 // @namespace    https://github.com/s1204IT/Disabled-Remover
-// @version      v1.0.0
+// @version      1.0.1
 // @description  Remove all "disabled" attributes in HTML tags
 // @author       Syuugo (@s1204IT)
 // @match        *://*/*
@@ -9,7 +9,4 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    Array.from(document.querySelectorAll('[disabled="disabled"], .disabled')).forEach(el => { el.removeAttribute('disabled'); el.classList.remove('disabled'); });
-})();
+Array.from(document.querySelectorAll('[disabled="disabled"], .disabled')).forEach(el => { el.removeAttribute('disabled'); el.classList.remove('disabled'); });
